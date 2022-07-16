8582da1a3b4f2fc75cb84b678fed44c508ebe01f
Adarshswain <61945619+Adarshswain@users.noreply.github.com>
2022-07-16 23:34:19 +0530

    first commit

---
+++ src/layout/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1> The Parking Management System</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
